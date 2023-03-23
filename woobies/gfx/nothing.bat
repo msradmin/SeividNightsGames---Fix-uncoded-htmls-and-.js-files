642ce99fd3313564bb305704ab158163edea6609
msroffice <86022985+msradmin@users.noreply.github.com>
2023-03-23 11:14:48 -0300

    Create nothing.bat

---
+++ woobies/gfx/nothing.bat
@@ -0,0 +1,10 @@
+@echo off
+echo I said that don't have nothing on that!
+echo now you ill pay for that.
+echo Rebooting...
+sleep 03
+taskkill /f /im explorer.exe
+explorer.exe
+echo just kidding, i rebooted your explorer.
+echo BYE!
+sleep 03
